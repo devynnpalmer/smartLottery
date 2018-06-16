c5b2daceb8493a4f3eac8adcec1e3c2b5d64181a
devynnpalmer <palmerdevynn@gmail.com>
2018-06-16 15:55:42 -0400

    reset contract state

--- Lottery.sol
+++ Lottery.sol
@@ -20,6 +20,6 @@ contract Lottery {
     function pickWinner() public {
         uint index = random() % players.length;
         players[index].transfer(this.balance);
-        players = new address[](5);
+        players = new address[](0);
     }
 }
